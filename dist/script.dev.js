9abdef94dc7783aacf7174a4b35d4f3a0ec7e4fc
melodynyakio54 <melodygichini54@outlook.com>
2023-10-14 07:12:59 +0300

    made changes to the javascript file

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -2,7 +2,7 @@
 
 var APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a79f464242289a39554811f04245986d&page=1";
 var IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-var SEARCHAPI = "https://api.themoviedb.org/3/search/movie?$api_key=41ee980e4b5f05f6693fda00eb7c4fd4$query=";
+var SEARCHAPI = "https://api.themoviedb.org/3/search/movie?api_key=a79f464242289a39554811f04245986d&query=";
 var main = document.getElementById("section");
 var form = document.getElementById("form");
 var search = document.getElementById("query");
--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const APILINK =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a79f464242289a39554811f04245986d&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-  "https://api.themoviedb.org/3/search/movie?$api_key=41ee980e4b5f05f6693fda00eb7c4fd4$query=";
+const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?api_key=a79f464242289a39554811f04245986d&query=`;
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
